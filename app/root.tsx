9e3b94b1440da795a910dcf0ef58be07055bea0a
yamanidev <elamine.yamani@gmail.com>
2024-02-05 12:36:39 +0100

    installed prettier and tailwind prettier plugin + formatted project

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,11 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import styles from "~/styles/global.css"
+import { cssBundleHref } from '@remix-run/css-bundle';
+import type { LinksFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import styles from '~/styles/global.css';
 
 export const links: LinksFunction = () => [
-  {rel: 'stylesheet', href: styles},
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: styles },
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : [])
 ];
 
 export default function App() {
