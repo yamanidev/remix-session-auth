af891647f7a0c374c6f25b389916ec34bdaf0d9e
yamanidev <elamine.yamani@gmail.com>
2024-02-05 13:12:11 +0100

    protected index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,30 +1,41 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
+import { requireUser } from '~/utils/guards.server';
+import { destroySession, getSession } from '~/utils/session.server';
 
-export const meta: MetaFunction = () => {
-  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  return [{ title: `Welcome ${data?.name} | Remix Session based Authentication` }];
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const user = await requireUser(request);
+  return user;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const session = await getSession(request.headers.get('Cookie'));
+
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await destroySession(session)
+    }
+  });
 };
 
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className="flex justify-center pt-52">
+      <div className="w-full max-w-lg rounded-lg bg-gray-800 px-10 py-10 shadow-2xl">
+        <p>Why hello there {data.name}!</p>
+        <Form method="post">
+          <button type="submit" className="btn btn-link">
+            Log out
+          </button>
+        </Form>
+      </div>
+    </main>
   );
 }
